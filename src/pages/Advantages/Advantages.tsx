f24efb7ff5f9955cf86d01b28b1995106af3c8ae
paoox <pao.arreola.g@gmail.com>
2023-08-21 04:17:08 -0600

    Add section advantages , target user, coments , and specific the relevant advantages of teh company

--- src/pages/Advantages/Advantages.tsx
+++ src/pages/Advantages/Advantages.tsx
@@ -1,33 +1,89 @@
-
+import img from "@/pages/assets/image 53.svg"
 export default function Advantages(){
     return(
-        <main className="bg-white md:h-[452px] w-full flex flex-col md:p-14 gap-14">
-          <div className="flex justify-center">  
-            <h1 className="text-4xl font-[acme]">¿Porqué Co-labora?</h1>
-          </div>
-          <section className="flex gap-5 text-center">
-            <div className="flex flex-col items-center p-3 basis-1/4">
-                <img src="@/pages/assets/spaceship-icon.webp" alt="Icono Cohete"  className="h-28 w-28"/>
-                <h2 className="text-4xl font-[acme] text-primary">Experiencias únicas</h2>
-                <p className="text-lg font-[poppins] text-blue_800">Cada vez que rentes estas abriendo la ventana a una experiencia nueva</p>
+        <>
+        <section className="bg-white md:h-[452px] w-full flex flex-col md:p-14 gap-14">
+            <div className="flex justify-center">
+                <h1 className="text-4xl font-[acme]">¿Porqué Co-labora?</h1>
             </div>
-            <div className="flex flex-col items-center p-3 basis-1/4">
-                <img src="@/pages/assets/Icon-Money.svg" alt="Icono bolsa con dinero"  className="h-28 w-28"/>
-                <h2 className="text-4xl font-[acme] text-primary">Adaptado a ti</h2>
-                <p className="text-lg font-[poppins] text-blue_800">Renta el lugar que cumpla con lo que necesitas</p>
+            <section className="flex gap-5 text-center">
+                <div className="flex flex-col items-center p-3 basis-1/4">
+                    <img src="@/pages/assets/spaceship-icon.webp" alt="Icono Cohete" className="h-28 w-28" />
+                    <h2 className="text-4xl font-[acme] text-primary">Experiencias únicas</h2>
+                    <p className="text-lg font-[poppins] text-blue_800">Cada vez que rentes estas abriendo la ventana a una experiencia nueva</p>
+                </div>
+                <div className="flex flex-col items-center p-3 basis-1/4">
+                    <img src="@/pages/assets/Icon-Money.svg" alt="Icono bolsa con dinero" className="h-28 w-28" />
+                    <h2 className="text-4xl font-[acme] text-primary">Adaptado a ti</h2>
+                    <p className="text-lg font-[poppins] text-blue_800">Renta el lugar que cumpla con lo que necesitas</p>
+                </div>
+                <div className="flex flex-col items-center p-3 basis-1/4">
+                    <img src="@/pages/assets/Icon-Users.svg" alt="Icono Usuarios" className="h-28 w-28" />
+                    <h2 className="text-4xl font-[acme] text-primary">Te conectamos</h2>
+                    <p className="text-lg font-[poppins] text-blue_800">Conoce nuevas personas cada día</p>
+                </div>
+                <div className="flex flex-col items-center p-3 basis-1/4">
+                    <img src="@/pages/assets/Icon-Compass.svg" alt="Icono Compass" className="h-28 w-28" />
+                    <h2 className="text-4xl font-[acme] text-primary">Explora</h2>
+                    <p className="text-lg font-[poppins] text-blue_800">Nuevo día nuevas oportunidades de viajar con tu trabajo por la ciudad</p>
+                </div>
+
+            </section>
+        </section>
+        <section className="bg-secondary md:h-[452px] w-full flex flex-col md:p-14 gap-14">
+            <div className="flex gap-5">
+                <div className="flex basis-1/4 gap-5 ">
+                    <img src="@/pages/assets/Boton-back.svg" alt="Boton Back" />
+                    <img src="@/pages/assets/Boton-back.svg-1" alt="Boton Next" />
+                </div>
+                <div className="basis-1/4 flex justify-end">
+                    <img src="@/pages/assets/Progress-circulos.svg" alt="Boton circulos"/>
+                </div>
             </div>
-            <div className="flex flex-col items-center p-3 basis-1/4">
-                <img src="@/pages/assets/Icon-Users.svg" alt="Icono Usuarios"  className="h-28 w-28"/>
-                <h2 className="text-4xl font-[acme] text-primary">Te conectamos</h2>
-                <p className="text-lg font-[poppins] text-blue_800">Conoce nuevas personas cada día</p>
-            </div>
-            <div className="flex flex-col items-center p-3 basis-1/4">
-                <img src="@/pages/assets/Icon-Compass.svg" alt="Icono Compass"  className="h-28 w-28"/>
-                <h2 className="text-4xl font-[acme] text-primary">Explora</h2>
-                <p className="text-lg font-[poppins] text-blue_800">Nuevo día nuevas oportunidades de viajar con tu trabajo por la ciudad</p>
-            </div>
-            
-          </section>
-        </main>
+            <section className="flex gap-24 md:px-[90px] ">
+                <div className="flex flex-col gap-5 md:basis-1/2 p-6">
+                    <div>
+                         <p className="text-lg font-[poppins] text-white">“ Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. ” </p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <div>
+                            <img src="@/pages/assets/image 53.svg" alt="Jean Lark" className="h-16 w-16 rounded-[87px]"/>
+                        </div>
+                        <div className="flex flex-col">
+                         <div className="flex gap-5">
+                          <p className="text-sm font-[poppins] font-semibold text-white">Jean Lark</p>
+                          <p className="text-[10px] font-[poppins] font-light text-white">Medico especialista en nutricion</p>
+                         </div>
+                        <div>
+                        <p className="text-sm font-[poppins] font-semibold text-white">Doctor</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <div className="gap-8 md:basis-1/2 p-6">
+                    <div className="flex my-5">
+                      <div className="md:border-l-4 px-3 md:basis-1/2">
+                        <p className="font-[poppins] text-[40px] text-blue_800 font-medium">204</p>
+                        <p className="font-[poppins] text-2xl text-blue_800 font-medium">Espacios  en renta</p>
+                      </div>
+                      <div className="md:border-l-4 px-3 md:basis-1/2">
+                        <p className="font-[poppins] text-[40px] text-blue_800 font-medium">+500</p>
+                        <p className="font-[poppins] text-2xl text-blue_800 font-medium">Usuarios</p>
+                      </div>
+                    </div>
+                    <div className="flex my-5">
+                    <div className="md:border-l-4 px-3 md:basis-1/2">
+                        <p className="font-[poppins] text-[40px] text-blue_800 font-medium">804</p>
+                        <p className="font-[poppins] text-2xl text-blue_800 font-medium">Clientes satisfechos</p>
+                    </div>
+                    <div className="md:border-l-4 px-3 md:basis-1/2">
+                        <p className="font-[poppins] text-[40px] text-blue_800 font-medium">+1000</p>
+                        <p className="font-[poppins] text-2xl text-blue_800 font-medium">Visitas mensuales</p>
+                    </div>
+                    </div>
+                </div>
+            </section>
+        </section>
+        </>
     )
 }
