7a183ea14ec6f46877828e459764ec1782ff05e9
DamianT <elmo@DESKTOP-OP1RDAI>
2023-08-21 17:05:43 -0600

    fixingg: error on merge

--- src/data/card-data.tsx
+++ src/data/card-data.tsx
@@ -1,19 +1,113 @@
-export const dataBD = {
-  name: "Carpinteria angel",
-  address:
-    "Calz.de la Ronda 99-loc.b, Ex hipodromo de la peravillo, Cuahutemoc,06250",
-  size: 72,
-  amenities: {
-    wifi: true,
-    pet: true,
-    parking: true,
-    aircondition: true,
-    cleaning: false,
-    reception: true,
+export const dataBD = [
+  {
+    name: "Carpinteria angel",
+    address:
+      "Calz.de la Ronda 99-loc.b, Ex hipodromo de la peravillo, Cuahutemoc,06250",
+    size: 72,
+    amenities: {
+      wifi: true,
+      pet: true,
+      parking: true,
+      aircondition: true,
+      cleaning: false,
+      reception: true,
+    },
+    rating: 4.26,
+    opinions: 24,
+    price: 250,
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,",
   },
-  rating: 5,
-  opinions: 12,
-  price: 250,
-  description:
-    "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,",
-};
+  {
+    name: "Tatuajes Roxy",
+    address: "Av Miguel Hidalgo 18,Tlalnepantla De Baz,Estado de Mexico,54000",
+    size: 25,
+    amenities: {
+      wifi: true,
+      pet: true,
+      parking: true,
+      aircondition: true,
+      cleaning: false,
+      reception: true,
+    },
+    rating: 2.14,
+    opinions: 8,
+    price: 250,
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,",
+  },
+  {
+    name: "Carpinteria angel",
+    address:
+      "Calz.de la Ronda 99-loc.b, Ex hipodromo de la peravillo, Cuahutemoc,06250",
+    size: 72,
+    amenities: {
+      wifi: true,
+      pet: true,
+      parking: true,
+      aircondition: true,
+      cleaning: false,
+      reception: true,
+    },
+    rating: 4.26,
+    opinions: 24,
+    price: 250,
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,",
+  },
+  {
+    name: "Tatuajes Roxy",
+    address: "Av Miguel Hidalgo 18,Tlalnepantla De Baz,Estado de Mexico,54000",
+    size: 25,
+    amenities: {
+      wifi: true,
+      pet: true,
+      parking: true,
+      aircondition: true,
+      cleaning: false,
+      reception: true,
+    },
+    rating: 2.14,
+    opinions: 8,
+    price: 250,
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,",
+  },
+  {
+    name: "Carpinteria angel",
+    address:
+      "Calz.de la Ronda 99-loc.b, Ex hipodromo de la peravillo, Cuahutemoc,06250",
+    size: 72,
+    amenities: {
+      wifi: true,
+      pet: true,
+      parking: true,
+      aircondition: true,
+      cleaning: false,
+      reception: true,
+    },
+    rating: 4.26,
+    opinions: 24,
+    price: 250,
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,",
+  },
+  {
+    name: "Dentista Javier",
+    address: "Av Miguel Hidalgo 75,Tlalnepantla De Baz,Estado de Mexico,12548",
+    size: 12,
+    amenities: {
+      wifi: true,
+      pet: true,
+      parking: true,
+      aircondition: true,
+      cleaning: false,
+      reception: true,
+    },
+    rating: 2.14,
+    opinions: 8,
+    price: 250,
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,",
+  },
+];
