d9ed6bd3bd4acf7e88ba3a8c096b0227299e17ea
paoox <pao.arreola.g@gmail.com>
2023-08-21 01:06:23 -0600

    Add branch feature- advantages, changes config tailwind

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <h1 className='bg-white'>Hola</h1>
       </body>
     </Html>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
 import type { Config } from 'tailwindcss'
 
+import type { Config } from "tailwindcss";
+
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#A54729",
+      secondary: "#E19C5B",
+      third: "#FACCAD",
+      white: "#FACCAD",
+      blue_800: "#37474F",
+      blue_700: "#455A64",
+      blue_500: "#607D8B",
+      blue_300: "#90A4AE",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
