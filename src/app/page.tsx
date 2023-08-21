67bc8ed814d49eaf73da33a81f662c0852bbb421
DamianT <elmo@DESKTOP-OP1RDAI>
2023-08-21 17:13:24 -0600

    fix: error on merge resolved from develop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Cards from "../components/Cards";
 import { dataBD } from "@/data/card-data";
+import Cards from "@/components/Cards";
 export default function Home() {
   return (
     <>
@@ -16,15 +16,64 @@ export default function Home() {
               href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer">
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
-              />
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Docs{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Find in-depth information about Next.js features and API.
+              </p>
+            </a>
+
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer">
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Learn{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Learn about Next.js in an interactive course with&nbsp;quizzes!
+              </p>
+            </a>
+
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer">
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Templates{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Explore the Next.js 13 playground.
+              </p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer">
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Deploy{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Instantly deploy your Next.js site to a shareable URL with
+                Vercel.
+              </p>
             </a>
           </div>
         </div>
@@ -106,7 +155,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      {/* <div className="flex flex-row flex-wrap">
+      <div className="flex flex-row flex-wrap">
         {dataBD.map((card, index) => (
           <div key={index} className="flex flex-row px-11 pb-5">
             <Cards
@@ -121,7 +170,7 @@ export default function Home() {
             />
           </div>
         ))}
-      </div> */}
+      </div>
     </>
   );
 }
