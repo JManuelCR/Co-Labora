95781727dd86af2194113a9840dea8eb0c4b3855
DamianT <elmo@DESKTOP-OP1RDAI>
2023-08-30 18:59:04 -0600

    add: font color changes from white to blue-700

--- src/components/CommentsUser.tsx
+++ src/components/CommentsUser.tsx
@@ -6,13 +6,13 @@ export default function CommentsUsers() {
     <section className="w-full bg-white md:p-14">
       <div className="flex justify-center md:my-[70px]">
         <div className="border-b-4 border-secondary">
-          <h1 className="md:text-4xl leading-loose font-[acme]">
+          <h1 className="text-blue_800 md:text-4xl leading-loose font-[acme] ">
             Lo que nuestros clientes opinan
           </h1>
         </div>
       </div>
       {
-        <div className="flex flex-row flex-wrap">
+        <div className="flex flex-row flex-wrap text-blue_800">
           {dataCommentsUsers.map((card, index) => (
             <div key={index} className="flex flex-row px-11 pb-5">
               <CardComments
