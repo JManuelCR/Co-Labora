8e29cca1a8f7fee2e6a2a2d0ee78a1522a6a81a4
paoox <pao.arreola.g@gmail.com>
2023-09-04 01:38:05 -0600

    add: import components NavBar and Footer

---
+++ src/app/createAccount/page.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/components/Navbar"
+import FooterMobile from "@/components/FooterMobile"
+import Footer from "@/components/Footer"
+
+export default function Login(){
+    return()
+}
