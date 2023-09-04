90e611624362912b8615e7f3802d8a76d6663337
paoox <pao.arreola.g@gmail.com>
2023-09-03 21:30:32 -0600

    add:Input Correo Electronico

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,14 @@ export default function Login () {
     return(
         <>
         <Navbar page="login"/>
-        <section className="mx-5 my-[74px] flex flex-col gap-[13px]">
-            <h1 className="font-acme text-titleMobil">Bienvenido a <span className="text-primary">Co-Labora</span></h1>
-            <p className="font-poppins   text-center text-suTitles">Encuentra tu espacio perfecto</p>
+        <section className="flex flex-col mx-5">
+            <div className="my-[74px] gap-[13px]">
+              <h1 className="font-acme text-titleMobil text-blue_800">Bienvenido a <span className="text-primary">Co-Labora</span></h1>
+              <p className="font-poppins text-center text-suTitles text-blue_700">Encuentra tu espacio perfecto</p>
+            </div>
+            <div className="mt-[17px]">
+                <input type="text" placeholder="Correo Electronico" className="flex rounded-[15px] border-2 border-primary w-full h-[65px] font-poppins text-[16px] text-blue_500 placeholder:p-[5px] placeholder:text-start"/>
+            </div>
         </section>
         </>
     )
