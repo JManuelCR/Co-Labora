0284877439db9ee6f934deea6cfc0891d62f8e14
paoox <pao.arreola.g@gmail.com>
2023-09-05 01:22:24 -0600

    add: text areas , text and responsive

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,7 @@ export default {
       fontFamily: {
         poppins: ["var(--font-poppins)"],
         acme: ["var(--font-acme)"],
+        roboto: ["var(--font-roboto)"],
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
