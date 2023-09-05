a9aad35543cdaf4b321f4249578dac6a7383113a
DamianT <elmo@DESKTOP-OP1RDAI>
2023-09-05 13:46:25 -0600

    add: footer component to in-rent page

--- src/app/in-rent/page.tsx
+++ src/app/in-rent/page.tsx
@@ -1,9 +1,19 @@
-import Navbar from "@/components/Navbar"
-export default function Rent () {
-    return(
-        <>
-        <Navbar page="in rent"/>
-        <h1>Yosoy una renta</h1>
-        </>
-    )
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import FooterMobile from "@/components/FooterMobile";
+export default function Rent() {
+  return (
+    <>
+      <Navbar page="in rent" />
+      <h1>Yos oy una renta</h1>
+      <div>
+        <footer className="hidden md:block">
+          <Footer />
+        </footer>
+        <footer className="block md:hidden">
+          <FooterMobile />
+        </footer>
+      </div>
+    </>
+  );
 }
