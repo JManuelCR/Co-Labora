41ed940eec9c1b92f3a6adf1263f2376488c5ceb
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2023-09-06 18:16:17 -0600

    add: stepper for reservation flow

--- src/app/in-rent/page.tsx
+++ src/app/in-rent/page.tsx
@@ -1,9 +1,25 @@
-import Navbar from "@/components/Navbar"
-export default function Rent () {
-    return(
-        <>
-        <Navbar page="in rent"/>
-        <h1>Yosoy una renta</h1>
-        </>
-    )
+"use client";
+import Navbar from "@/components/Navbar";
+import OwnStepper from "@/components/OwnStepper";
+export default function Rent() {
+  return (
+    <>
+      <Navbar page="in rent" />
+      <OwnStepper actualStep={2} />
+      <div className="w-full flex justify-center h-auto">
+        <div className="flex flex-col md:flex-row gap-[18px] md:gap-[100px]">
+          <button className="bg-primary rounded-lg px-[18px] py-1 w-[134px] h-[35px] buttonMobileShadow">
+            <span className="text-[14px] font-[600] leading-[27px] text-white tracking-[-0.28px]">
+              Siguiente
+            </span>
+          </button>
+          <button className="bg-white rounded-lg px-[18px] py-1 w-[134px] h-[35px] border border-primary buttonMobileShadow">
+            <span className="text-[14px] font-[600] leading-[27px] text-primary tracking-[-0.28px]">
+              Ir atrás
+            </span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
