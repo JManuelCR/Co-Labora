fd32a7f03892c1649e50637c5bb282cae8487b28
DamianT <elmo@DESKTOP-OP1RDAI>
2023-09-12 18:15:42 -0600

    add: react hook form on login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,21 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import FooterMobile from "@/components/FooterMobile";
 import Footer from "@/components/Footer";
-
+import { useForm, SubmitHandler } from "react-hook-form";
+import inputs from "@/types/inputs.types";
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<inputs>();
+  const onSubmit: SubmitHandler<inputs> = (data) => console.log(data);
   return (
     <>
       <Navbar page="login" />
-      <section className=" w-100% h-100% md:w-full md:h-100% flex items-center justify-center lg:mt-[212px] ">
+      <section className=" w-100% h-100% md:w-full md:h-100% flex items-center justify-center lg:mt-[212px] text-blue_800">
         <section className="flex flex-col mx-5 md:w-[440px] md:h-[510px] windowXl">
           <div className="my-[74px] lg:my-0 gap-[15px]">
             <h1 className="font-acme text-titleMobil text-blue_800 text-center">
@@ -16,22 +25,29 @@ export default function Login() {
               Encuentra tu espacio perfecto
             </p>
           </div>
-          <form className="mt-[17px]">
+          <form className="mt-[17px]" onSubmit={handleSubmit(onSubmit)}>
             <input
               type="email"
-              name="email"
               placeholder="Correo Electronico"
-              className="flex rounded-[15px] border-2 border-primary w-full h-[65px] font-poppins text-[16px] text-blue_500 placeholder:p-[10px] p-[15px] placeholder:text-start focus:outline-0 focus:border-primary required my-5"
+              className="flex rounded-[15px] border-2 border-primary w-full h-[65px] font-poppins text-[16px] text-blue_500 placeholder:p-[10px] p-[15px] placeholder:text-start focus:outline-0 focus:border-primary  my-5"
+              {...register("email", {
+                required: "Este campo es obligatorio",
+                minLength: 10,
+              })}
             />
+            <p>{errors.email?.message}</p>
             <div>
               <input
                 type="password"
-                name="password"
+                {...register("password", {
+                  required: "Este campo es obligatorio",
+                })}
                 placeholder="Contraseña"
-                className="flex rounded-[15px] border-2 border-primary w-full h-[65px] font-poppins text-[16px] text-blue_500 placeholder:p-[10px] p-[15px] placeholder:text-start mt-5 focus:outline-0 focus:border-primary required my-5"
+                className="flex rounded-[15px] border-2 border-primary w-full h-[65px] font-poppins text-[16px] text-blue_500 placeholder:p-[10px] p-[15px] placeholder:text-start mt-5 focus:outline-0 focus:border-primary  my-5"
               />
+              <p>{errors.password?.message}</p>
             </div>
-            <div className="flex my-[50px] gap-[15px] items-center">
+            <div className="flex my-[25px] gap-[10px] items-center">
               <input
                 type="radio"
                 name="rememberPassword"
@@ -42,7 +58,7 @@ export default function Login() {
                 Recordar contraseña
               </p>
             </div>
-            <div className="m-16 flex justify-center">
+            <div className="m-10 flex justify-center">
               <button className="bg-primary rounded-lg w-[200px] h-[60px]">
                 <p className="font-poppins text-suTitles text-white">Ingresa</p>
               </button>
---
+++ src/types/inputs.types.ts
@@ -0,0 +1,4 @@
+export default interface inputs {
+  email: string;
+  password: string;
+}
