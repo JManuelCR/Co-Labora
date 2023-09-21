d0efe6717b866117ea059671dc8b8312c9f8319c
DamianT <elmo@DESKTOP-OP1RDAI>
2023-09-21 16:54:57 -0600

    delete: removed google maps key

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -23,7 +23,7 @@ const amenities = {
 };
 
 export default function Detail() {
-  const key = "";
+  const key = ""; // ! aqui va la llave de google maps
   const libraries = useMemo(() => ["places"], []);
   const mapCenter = useMemo(
     () => ({ lat: 19.420831779520512, lng: -99.13501950384934 }),
