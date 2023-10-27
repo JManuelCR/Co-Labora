411d99a05d6bdbcfac98e7c27910bfee14474080
DamianT <elmo@DESKTOP-OP1RDAI>
2023-10-27 13:36:35 -0600

    fix: changed localstorage in service folder to cookies

--- src/services/prperty.service.ts
+++ src/services/prperty.service.ts
@@ -1,29 +1,30 @@
+import { getCookie } from "cookies-next";
 import { fetchWrapper } from "./fetch-wrapper";
-const apiUri =  'https://co-labora-backend.jmanuelc.dev';
-const token = localStorage.getItem('token')
+const apiUri = "https://co-labora-backend.jmanuelc.dev";
+const token = getCookie("token");
 
 export const propertyService = {
   getAll,
   getById,
   create,
   update,
-  delete: _delete
+  delete: _delete,
 };
 
-const baseUrl = `${apiUri}/property`
+const baseUrl = `${apiUri}/property`;
 
-function getAll(){
+function getAll() {
   return fetchWrapper.get(baseUrl);
 }
-function getById(id: any){
-  return fetchWrapper.get(`${baseUrl}/${id}`)
+function getById(id: any) {
+  return fetchWrapper.get(`${baseUrl}/${id}`);
 }
-function create(body: any){
+function create(body: any) {
   return fetchWrapper.postProperty(baseUrl, body, token);
 }
-function update(params: any, id: any){
+function update(params: any, id: any) {
   return fetchWrapper.patch(`${baseUrl}/${id}`, params, token);
 }
-function _delete(id: any){
-  return fetchWrapper.delete(`${baseUrl}/${id}`, token)
+function _delete(id: any) {
+  return fetchWrapper.delete(`${baseUrl}/${id}`, token);
 }
