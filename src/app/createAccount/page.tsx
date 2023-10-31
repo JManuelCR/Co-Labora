c21e138107df40b3c228250098c11a6f4cf95235
DamianT <elmo@DESKTOP-OP1RDAI>
2023-10-30 20:32:11 -0600

    add: register user stepper working

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -3,11 +3,12 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import inputs from "@/types/inputs.types";
 import { FormEvent, useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 export default function CreateAccount({ props }: any) {
   const [type, setType] = useState("user");
   const [activeButton, setActiveButton] = useState("user");
   const [passShow, setPassShow] = useState(false);
-  const [valid, setValid] = useState(true);
+  const [valid, setValid] = useState(false);
 
   const handleTypeUser = () => {
     setType("user"), setActiveButton("user");
@@ -36,7 +37,7 @@ export default function CreateAccount({ props }: any) {
   };
 
   const onSubmit: SubmitHandler<inputs> = (data) => {
-    if (valid) {
+    if (valid && data.email !== undefined) {
       remove();
       const userType = type;
       const toPass = {
@@ -63,10 +64,24 @@ export default function CreateAccount({ props }: any) {
         .catch((error) => {
           console.log("fetch error", error);
         });
+      const button = document.getElementById("step-register");
+      if (button) {
+        button.click();
+      }
       props(toPass);
     } else {
       // Notify the user that the password is invalid
       alert("Please enter a valid password.");
+      toast.error("Error al verificar tu codigo", {
+        position: "top-center",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
     }
   };
 
@@ -82,9 +97,21 @@ export default function CreateAccount({ props }: any) {
     const isValid = passRegex.test(input);
     setValid(isValid);
   };
-
+  console.log("esto es isvalid", valid);
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <section className=" w-100%  md:w-full md:h-100% flex items-center justify-center mb-40">
         <section className="flex flex-col mx-5 md:w-[440px] md:h-[510px] windowXl">
           <div className=" lg:my-0 gap-[13px]">
@@ -95,25 +122,47 @@ export default function CreateAccount({ props }: any) {
               Encuentra tu espacio perfecto
             </p>
           </div>
-
-          <div className="flex justify-between text-center mt-6">
-            <div className="font-poppins text-suTitles text-black ">
+          <div className="flex justify-center mt-5">
+            <h3 className="text-center font-poppins text-slate-600 text-suTitles">
+              Como te registramos?
+            </h3>
+          </div>
+          <div className="flex justify-between text-center ">
+            <div className="font-poppins text-suTitles text-black relative group">
               <button
                 onClick={handleTypeUser}
-                className="focus:underline focus:text-primary">
+                className={`focus:underline focus:text-primary ${
+                  activeButton === "user" ? "text-primary underline" : ""
+                }`}>
                 Soy Usuario
               </button>
+              <div
+                className={`absolute bg-secondary text-white p-2 rounded-lg mt-2 opacity-0 invisible group-hover:opacity-100 group-hover:visible whitespace-pre-line ${
+                  activeButton === "user" ? "opacity-100 visible" : ""
+                }`}>
+                Me interesa rentar espacios únicamente
+              </div>
             </div>
-            <div className="font-poppins text-suTitles text-black ">
+            <div className="font-poppins text-suTitles text-black relative group">
               <button
                 onClick={handleTypeSpace}
-                className="focus:underline focus:text-primary">
+                className={`focus:underline focus:text-primary ${
+                  activeButton === "space" ? "text-primary underline" : ""
+                }`}>
                 Soy Negocio
               </button>
+              <div
+                className={`absolute bg-secondary text-white p-2 rounded-lg mt-2 opacity-0 invisible group-hover:opacity-100 group-hover:visible whitespace-pre-line ${
+                  activeButton === "space" ? "opacity-100 visible" : ""
+                }`}>
+                Me interesa publicar mis espacios para rentarlos
+              </div>
             </div>
           </div>
 
-          <form className="mt-[30px]" onSubmit={handleSubmit(onSubmit)}>
+          <form
+            className="flex flex-col mt-[30px] justify-center items-center"
+            onSubmit={handleSubmit(onSubmit)}>
             <input
               type="email"
               {...register("email", {
@@ -124,7 +173,7 @@ export default function CreateAccount({ props }: any) {
               className="flex rounded-[15px] border-2 border-primary w-full h-[65px] font-poppins text-[16px] text-blue_500 placeholder:p-[10px] p-[15px] placeholder:text-start focus:outline-0 focus:border-primary required"
             />
             <p className="text-primary">{errors.email?.message}</p>
-            <div className="my-5 flex rounded-[15px] border-2 border-primary font-poppins text-[16px] text-blue_500 px-3">
+            <div className="my-5 flex rounded-[15px] border-2 border-primary font-poppins text-[16px] text-blue_500 px-3 w-full">
               <input
                 id="password1"
                 type={passShow ? "text" : "password"}
@@ -150,23 +199,6 @@ export default function CreateAccount({ props }: any) {
               </p>
             ) : null}
             <p className="text-primary">{errors.email?.message}</p>
-            <div className="my-5 flex rounded-[15px] border-2 border-primary font-poppins text-[16px] text-blue_500 px-3">
-              <input
-                id="2password"
-                type={passShow ? "text" : "password"}
-                {...register("confirmPassword", {
-                  required: "Este campo es obligatorio",
-                  pattern:
-                    /^(?=.*[!@#$%^&*()_+])[A-Za-z0-9!@#$%^&*()_+]+(?=.*[A-Z])(?=.*\d).+$/,
-                  minLength: 8,
-                })}
-                placeholder="Confirmar contraseña"
-                className="flex w-full focus:outline-0 focus:border-primary my-5 "
-              />
-              <button onClick={tooglePass} form="2password">
-                <p className="text-blue_800 underline">Mostrar</p>
-              </button>
-            </div>
             <div className="flex my-3 gap-[15px]"></div>
             <div className="m-3 flex justify-center "></div>
             <div className="h-1 w-full bg-primary mb-[11px] "></div>
@@ -180,7 +212,12 @@ export default function CreateAccount({ props }: any) {
             <button
               id="submit-user-register"
               type="submit"
-              disabled={!valid}></button>
+              disabled={!valid}
+              className={`rounded-lg px-[18px] py-1 w-[134px] h-[35px] buttonMobileShadow  ${
+                !valid ? "bg-gray" : "bg-primary"
+              }`}>
+              Siguiente
+            </button>
           </form>
         </section>
       </section>
